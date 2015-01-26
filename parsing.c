c68cade7b4bcea61716658f83b4fe4b92c215332
niberg <niberg@users.noreply.github.com>
2015-01-26 17:55:34 +0100

    Simple reverse polish calculator works

--- parsing.c
+++ parsing.c
@@ -27,6 +27,40 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+/* Use operator string to see which operation to perform */
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  if (strcmp(op, "%") == 0) { return x % y; }
+  if (strcmp(op, "^") == 0) { return (int) pow((double) x, y); }
+  return 0;
+}
+long eval(mpc_ast_t* t) {
+  
+  /* If tagged as number return it directly. */ 
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+  
+  /* The operator is always second child. */
+  char* op = t->children[1]->contents;
+  
+  /* We store the third child in `x` */
+  long x = eval(t->children[2]);
+  
+  /* Iterate the remaining children and combining. */
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+  
+  return x;  
+}
+
+
 int main(int argc, char** argv) {
 
     /* Create Some Parsers */
@@ -39,12 +73,12 @@ int main(int argc, char** argv) {
     mpca_lang(MPCA_LANG_DEFAULT,
             "                                                   \
             number   : /-?[0-9]+/ ;                             \
-            operator : '+' | '-' | '*' | '/' ;                  \
+            operator : '+' | '-' | '*' | '/' | '%' | '^' ;      \
             expr     : <number> | '(' <operator> <expr>+ ')' ;  \
             lispy    : /^/ <operator> <expr>+ /$/ ;             \
             ",
             Number, Operator, Expr, Lispy);
-    puts("Lispy Version 0.0.0.0.1");
+    puts("Lispy Version 0.0.0.0.3");
     puts("Press Ctrl+c to Exit\n");
 
     while (1) {
@@ -53,7 +87,17 @@ int main(int argc, char** argv) {
         char* input = readline("lispy> ");
         add_history(input);
 
-        printf("No you're a %s\n", input);
+        /* Attempt to Parse the user Input */
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+            long result = eval(r.output);
+            printf("%li\n", result);
+            mpc_ast_delete(r.output);
+        } else {
+            /* Otherwise Print the Error */
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
         free(input);
 
     }
